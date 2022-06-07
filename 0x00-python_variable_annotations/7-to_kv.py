b79da536fc2cd6cfa4c0c419efd47244e6dfacc6
ecastan960 <ecastan1@eafit.edu.co>
2022-06-06 21:57:08 -0500

    [00] Upload files

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""7. Complex types - string and int/float to tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """type-annotated function
+
+    Args:
+        k (str): string
+        v (Union[int, float]): int or float number
+
+    Returns:
+        Tuple[str, float]: tuple with string and square of v
+    """    
+    return (k, v ** 2)
