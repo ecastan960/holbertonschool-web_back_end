8732097aab241d34680acf48a0ecd82a47c8fc32
ecastan960 <ecastan1@eafit.edu.co>
2022-06-06 20:39:13 -0500

    [00] Upload files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""1. Basic annotations - concat
+    """
+
+
+def concat(str1: str, str2: str) -> str:
+    """type-annotated function
+
+    Args:
+        str1 (str): string 1
+        str2 (str): string 2
+    Returns:
+        str: concatenated string
+    """    
+    return str1 + str2
