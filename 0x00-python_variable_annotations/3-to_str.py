20c7ef47af01cbb361924fa2c378ed375c2f8d56
ecastan960 <ecastan1@eafit.edu.co>
2022-06-06 20:56:27 -0500

    [00] Upload files

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""3. Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """type-annotated function
+
+    Args:
+        n (float): float number
+
+    Returns:
+        str: string representation of n
+    """
+    return str(n)
