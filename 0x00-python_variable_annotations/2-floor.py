802b14d1883ee265bb972100887b4fa49ecad60d
ecastan960 <ecastan1@eafit.edu.co>
2022-06-06 20:51:01 -0500

    [00] Upload files

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""2. Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """type-annotated function
+
+    Args:
+        n (float): float number
+
+    Returns:
+        float: floor of n
+    """
+    return math.floor(n)
