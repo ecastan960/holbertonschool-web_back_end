07d419a019334f6fdfb1350f589f7d0a253dda3f
ecastan960 <ecastan1@eafit.edu.co>
2022-06-06 20:32:27 -0500

    [00] Upload files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""0. Basic annotations - add
+    """
+
+
+def add(a: float, b: float) -> float:
+    """type-annotated function
+
+    Args:
+        a (float): float number
+        b (float): float number
+
+    Returns:
+        float: sum of a and b as float
+    """
+    return a + b
