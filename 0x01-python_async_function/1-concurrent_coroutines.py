f7dce9940ecbaacdbe0dd33422636c9dd4c14917
ecastan960 <ecastan1@eafit.edu.co>
2022-06-07 21:48:45 -0500

    [01] pep8 fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """_summary_
 
+    Args:
+        n (int): _description_
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        List[float]: _description_
+    """
     delays = []
     for i in range(n):
         delays.append(await wait_random(max_delay))
