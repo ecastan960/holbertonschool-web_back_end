f087ab204fbf032385b6f6484087480349320fcd
ecastan960 <ecastan1@eafit.edu.co>
2022-06-07 22:02:07 -0500

    [00] Upload files

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import time
+import asyncio
+wait_n = __import__('1-wait_n').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        float: _description_
+    """
+    t = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - t
+    return total_time / n
