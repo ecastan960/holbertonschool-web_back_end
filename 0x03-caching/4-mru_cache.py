2fbcd6b72d1ed4931fdacc3123ef9a2f1ae1fb65
ecastan960 <ecastan1@eafit.edu.co>
2022-06-10 21:57:13 -0500

    [00] Upload files

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """_summary_
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+    def __init__(self):
+        super().__init__()
+        self.head = 'head'
+        self.tail = 'tail'
+        self.next = {}
+        self.prev = {}
+        self.handle(self.head, self.tail)
+
+    def handle(self, head, tail):
+        """_summary_
+
+        Args:
+            head (_type_): _description_
+            tail (_type_): _description_
+        """
+        self.next[head] = tail
+        self.prev[tail] = head
+
+    def _remove(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+        """
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def _add(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.prev[self.tail]))
+            self._remove(self.prev[self.tail])
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key and item:
+            if key in self.cache_data:
+                self._remove(key)
+            self._add(key, item)
+
+    def get(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self._add(key, value)
+            return value
+        if key is None or self.cache_data.get(key) is None:
+            return None
