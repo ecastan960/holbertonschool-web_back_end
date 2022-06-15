f9f1b917e92a5085d9216e621aafc49c2e2effcb
ecastan960 <ecastan1@eafit.edu.co>
2022-06-14 20:23:50 -0500

    [00] Upload files

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import csv
+import math import floor
+from typing import Any, Dict, List
+
+
+def index_range(page: int, page_size: int):
+    """_summary_
+
+    Args:
+        page (int): _description_
+        page_size (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """_summary_
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            List[List]: _description_
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        start, end = index_range(page, page_size)
+        if start >= len(self.dataset()):
+            return []
+        pages = self.dataset()
+        return pages[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """_summary_
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            Dict[str, Any]: _description_
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        pages = floor(len(self.dataset()) / page_size)
+        return {'page_size': len(self.get_page(page, page_size)),
+                'page': page,
+                'data': self.get_page(page, page_size),
+                'next_page': page + 1 if page + 1 < pages else None,
+                'prev_page': page - 1 if page > 1 else None,
+                'total_pages': pages
+                }
