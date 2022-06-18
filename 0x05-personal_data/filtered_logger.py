e1ec11d2b7bbad3475984f03184d94a3d854857b
ecastan960 <ecastan1@eafit.edu.co>
2022-06-17 22:10:11 -0500

    [02] pep8 fix

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """_summary_
 
     Args:
