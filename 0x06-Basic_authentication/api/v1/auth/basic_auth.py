53d3fc9517b00beb8ae704955d47c7f3005f7c68
ecastan960 <ecastan1@eafit.edu.co>
2022-06-21 23:49:09 -0500

    [00] Upload files

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -70,3 +70,23 @@ class BasicAuth(Auth):
         userEmail = user[0]
         userPassword = user[1]
         return userEmail, userPassword
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """_summary_
+
+        Args:
+            self (_type_): _description_
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            return None
