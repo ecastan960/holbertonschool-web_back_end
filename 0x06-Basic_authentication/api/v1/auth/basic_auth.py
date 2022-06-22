85745b36b329d4ee684707d5045f0e4fa4086dfd
ecastan960 <ecastan1@eafit.edu.co>
2022-06-21 23:17:48 -0500

    [00] Upload files

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,22 @@ class BasicAuth(Auth):
     Args:
         Auth (_type_): _description_
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """_summary_
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        value = authorization_header.split(' ')
+        return value[1]
