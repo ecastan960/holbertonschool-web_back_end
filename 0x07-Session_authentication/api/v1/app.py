4b9b728f872def72e6698bec3f0747481a444801
ecastan960 <ecastan1@eafit.edu.co>
2022-06-23 18:55:11 -0500

    [01] condition fix

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -66,8 +66,6 @@ def before_request() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) is None:
-        abort(401)
     if auth.session_cookie(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
