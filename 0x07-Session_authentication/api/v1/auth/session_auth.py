c349f2ca5b0e85db29fa13bf8f761fc8d740b59b
ecastan960 <ecastan1@eafit.edu.co>
2022-06-23 20:17:05 -0500

    [00] Upload files

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -46,3 +46,18 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """_summary_
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        session = self.session_cookie(request)
+        if session is None:
+            return None
+        user = self.user_id_by_session_id(session)
+        return User.get(user)
