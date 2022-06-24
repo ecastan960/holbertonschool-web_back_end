a87f9547dea5287467e4dcaa577e8c001f86fa87
ecastan960 <ecastan1@eafit.edu.co>
2022-06-23 21:11:06 -0500

    [01] Using try

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -20,8 +20,9 @@ def login():
         return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({'email': email})
-    if not users:
+    try:
+        users = User.search({'email': email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
     for user in users:
         if not user.is_valid_password(password):
