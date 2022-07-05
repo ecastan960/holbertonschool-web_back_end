864a4b6ed7359bc93792c8d3b7925eb5272638c3
ecastan960 <ecastan1@eafit.edu.co>
2022-07-04 21:14:33 -0500

    [08] auth upload

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -11,8 +11,13 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-    """
-    Hashes str password
+    """_summary_
+
+    Args:
+        password (str): _description_
+
+    Returns:
+        bytes: _description_
     """
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt())
