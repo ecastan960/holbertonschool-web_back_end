df3793cd5040c63555f0db6c2974acb38d80db1e
ecastan960 <ecastan1@eafit.edu.co>
2022-07-04 22:07:08 -0500

    [18] update app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,9 +4,10 @@
 Returns:
     _type_: _description_
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from auth import Auth
 from flask.globals import request
+from flask.helpers import make_response
 
 AUTH = Auth()
 
@@ -38,6 +39,26 @@ def users():
             return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        result = make_response(jsonify({"email": email,
+                                        "message": "logged in"}))
+        result.set_cookie('session_id', session_id)
+        return result
+    else:
+        abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
