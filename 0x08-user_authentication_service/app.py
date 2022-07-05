6850ea42fd5435663b4a7c16f7c3770f288cccb6
ecastan960 <ecastan1@eafit.edu.co>
2022-07-04 21:30:01 -0500

    [11] upload app

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+from flask import Flask, jsonify, abort, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def greetings():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return jsonify({'message': 'Bienvenue'})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
