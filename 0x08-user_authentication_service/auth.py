c04bb1244e4d78e22c6a23502e3a2a68267b928d
ecastan960 <ecastan1@eafit.edu.co>
2022-07-05 04:42:43 -0500

    [19] update auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -51,3 +51,24 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """_summary_
+
+        Args:
+            email (str): _description_
+            password (str): _description_
+
+        Returns:
+            bool: _description_
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                if bcrypt.checkpw(
+                        password.encode('utf-8'),
+                        user.hashed_password):
+                    return True
+                return False
+        except Exception:
+            return False
