0d83e3cd8541ce9be0540db59430ac5fef15c706
ecastan960 <ecastan1@eafit.edu.co>
2022-07-05 05:50:42 -0500

    [23] update files

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,7 +4,7 @@
 Returns:
     _type_: _description_
 """
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, redirect
 from auth import Auth
 from flask.globals import request
 from flask.helpers import make_response
@@ -59,6 +59,70 @@ def login():
     else:
         abort(401)
 
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None or session_id is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None or session_id is None:
+        abort(403)
+    else:
+        return jsonify({"email": user.email}), 200
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    email = request.form['email']
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    email = request.form['email']
+    reset_token = request.form['reset_token']
+    new_password = request.form['new_password']
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({
+            "email": email,
+            "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -116,3 +116,55 @@ class Auth:
             return None
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int):
+        """_summary_
+
+        Args:
+            user_id (int): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        try:
+            return self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """_summary_
+
+        Args:
+            email (str): _description_
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            str: _description_
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """_summary_
+
+        Args:
+            reset_token (str): _description_
+            password (str): _description_
+
+        Raises:
+            ValueError: _description_
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
