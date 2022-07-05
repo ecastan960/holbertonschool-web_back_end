5a8433cd0d35b1c4224bedf35db73f8d7ea9f099
ecastan960 <ecastan1@eafit.edu.co>
2022-07-04 21:44:39 -0500

    [17] update app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -5,7 +5,10 @@ Returns:
     _type_: _description_
 """
 from flask import Flask, jsonify
+from auth import Auth
+from flask.globals import request
 
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -18,6 +21,23 @@ def greetings():
         _type_: _description_
     """
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        user = AUTH.register_user(email, password)
+        if user:
+            return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
