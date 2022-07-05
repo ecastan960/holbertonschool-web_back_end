d11ecb12d506d7f601115a87299d04c2216eedae
ecastan960 <ecastan1@eafit.edu.co>
2022-07-04 21:20:37 -0500

    [09] auth update

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
 import bcrypt
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -21,3 +22,33 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt())
+
+
+class Auth:
+    """_summary_
+    """
+
+    def __init__(self):
+        """_summary_
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """_summary_
+
+        Args:
+            email (str): _description_
+            password (str): _description_
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            User: _description_
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(
+                "User {} already exists".format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
