52d60b5923f5c40d97bcd1c4406a0b94bdd3e39b
ecastan960 <ecastan1@eafit.edu.co>
2022-07-07 00:04:44 -0500

    [06] update filtered_logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@
 import re
 import logging
 from typing import List
+import os
+import mysql.connector
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
@@ -66,3 +68,16 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """_summary_
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: _description_
+    """
+    return mysql.connector.connect(
+        host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.environ.get('PERSONAL_DATA_DB_NAME', 'root'),
+        user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
+        password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
