46641fda47dcb25b701ea2c177558f92370d628e
ecastan960 <ecastan1@eafit.edu.co>
2022-07-07 00:09:49 -0500

    [09] update encrypt file

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -17,3 +17,16 @@ def hash_password(password: str) -> bytes:
         bytes: _description_
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """_summary_
+
+    Args:
+        hashed_password (bytes): _description_
+        password (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
