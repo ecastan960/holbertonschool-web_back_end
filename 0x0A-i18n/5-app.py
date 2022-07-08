a9e9501a1174a99e138c9b539634aae5985538da
ecastan960 <ecastan1@eafit.edu.co>
2022-07-07 21:43:48 -0500

    [07] upload files

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@app.route('/')
+def root():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return render_template('1-index.html')
+
+
+class Config(object):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    """ to determine the best match with our supported languages """
+    lang = request.args.get('locale')
+    configLang = app.config['LANGUAGES']
+    if lang in configLang:
+        return lang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    try:
+        userId = request.args.get('login_as')
+        return users[int(userId)]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """_summary_
+    """
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run()
