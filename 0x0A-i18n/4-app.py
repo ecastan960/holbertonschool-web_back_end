ee13971f372f7cd5ebc67be219332cfbd090518c
ecastan960 <ecastan1@eafit.edu.co>
2022-07-07 21:31:43 -0500

    [06] upload files

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def root():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return render_template('1-index.html')
+
+
+class Config(object):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    """ to determine the best match with our supported languages """
+    lang = request.args.get('locale')
+    configLang = app.config['LANGUAGES']
+    if lang in configLang:
+        return lang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
