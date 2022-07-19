7666e0d6a9cc415d14c62a803a0f63103efb61b6
ecastan960 <ecastan1@eafit.edu.co>
2022-07-19 06:01:17 -0500

    [00] upload files

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Redis exercise task 0
+"""
+
+import redis
+import uuid
+from typing import Optional, Callable, Union
+
+
+class Cache:
+    """_summary_
+    """
+
+    def __init__(self):
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
+
+    def store(self, data) -> str:
+        """_summary_
+
+        Args:
+            data (_type_): _description_
+
+        Returns:
+            str: _description_
+        """
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
+
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Union[str, bytes, int]:
+        """_summary_
+
+        Args:
+            key (str): _description_
+            fn (Optional[Callable]): _description_
+
+        Returns:
+            Union[str, bytes, int]: _description_
+        """
+        data = self.__redis.get(key)
+        if fn:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str) -> str:
+        """_summary_
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        return self.__redis.get(key).decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """_summary_
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            int: _description_
+        """
+        data = self.__redis.get(key)
+        try:
+            data = int(data.decode("utf-8"))
+        except:
+            data = 0
+        return data
