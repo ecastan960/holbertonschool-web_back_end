87bad702fb093c4b2ecc92d6cac5902ecd25aaec
ecastan960 <ecastan1@eafit.edu.co>
2022-07-21 21:19:40 -0500

    [07] upload task 7

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,24 @@
+-- Task 7
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+delimiter / /
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT) BEGIN DECLARE avg_score FLOAT;
+
+SET
+	avg_score = (
+		SELECT
+			AVG(score)
+		FROM
+			corrections AS C
+		WHERE
+			C.user_id = user_id
+	);
+
+UPDATE
+	users
+SET
+	average_score = avg_score
+WHERE
+	id = user_id;
+
+END;
