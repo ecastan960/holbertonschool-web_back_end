7c0d41a972f2d325f641f51f50fd6e6cd151c106
ecastan960 <ecastan1@eafit.edu.co>
2022-07-26 20:50:50 -0500

    [24] test task 101

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import pymongo
+
+
+def top_students(mongo_collection):
+    """ method docs """
+    return mongo_collection.aggregate([
+        {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ])
