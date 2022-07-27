52aba6d81c085d892f3bd62116bfb9d4641a6d17
ecastan960 <ecastan1@eafit.edu.co>
2022-07-26 20:06:18 -0500

    [22] test task 12

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -7,22 +7,23 @@ from pymongo import MongoClient
 def logger(a: dict) -> int:
     """_summary_
     """
-    return MongoClient('mongodb://127.0.0.1:27017').logs.nginx.count_documents(a)
+    client = 'mongodb://127.0.0.1:27017'
+    return MongoClient(client).logs.nginx.count_documents(a)
 
 
 def main():
     """_summary_
     """
-    info = [f"{ logger({}) } logs", "Methods:",
-            f"\tmethod GET: { logger({'method': 'GET'}) }",
-            f"\tmethod POST: { logger({'method': 'POST'}) }",
-            f"\tmethod PUT: {logger({'method': 'PUT'})}",
-            f"\tmethod PATCH: {logger({'method': 'PATCH'})}",
-            f"\tmethod DELETE: {logger({'method': 'DELETE'})}",
-            f"{logger({'method': 'GET', 'path': '/status'})} status check"]
+    stats = [f"{ logger({}) } logs", "Methods:",
+             f"\tmethod GET: { logger({'method': 'GET'}) }",
+             f"\tmethod POST: { logger({'method': 'POST'}) }",
+             f"\tmethod PUT: {logger({'method': 'PUT'})}",
+             f"\tmethod PATCH: {logger({'method': 'PATCH'})}",
+             f"\tmethod DELETE: {logger({'method': 'DELETE'})}",
+             f"{logger({'method': 'GET', 'path': '/status'})} status check"]
 
-    for i in info:
-        print(i)
+    for stat in stats:
+        print(stat)
 
 
 if __name__ == "__main__":
