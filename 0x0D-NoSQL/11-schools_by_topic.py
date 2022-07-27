5b8ef8962a72a01742bf48fe9dd94cd9c6c537ba
ecastan960 <ecastan1@eafit.edu.co>
2022-07-26 19:55:25 -0500

    [18] test task 11

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """_summary_
+    """
+    return mongo_collection.find({"topic": topic})
