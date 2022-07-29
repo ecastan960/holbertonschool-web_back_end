9a43ee5ae277943c83ce30c7b461888000bbd8ab
ecastan960 <ecastan1@eafit.edu.co>
2022-07-28 21:19:59 -0500

    [11] task 05

--- 0x0E-ES6_basic/5-spread-operator.js
+++ 0x0E-ES6_basic/5-spread-operator.js
@@ -1,1 +1,3 @@
-export default (array1, array2, string) => array1.concat(array2, string.split(''));
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
