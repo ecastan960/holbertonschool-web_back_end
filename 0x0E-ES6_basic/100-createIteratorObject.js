14e0590ca1a688b1eb614a22d03a361ca1f3731d
ecastan960 <ecastan1@eafit.edu.co>
2022-07-28 23:23:31 -0500

    [22] task 100

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,3 @@
 export default function createIteratorObject(report) {
-  return Object.values(report.allEmployees).reduce((acc, val) => acc + val, []);
+  return Object.values(report.allEmployees).reduce((acc, next) => acc.concat(next), []);
 }
