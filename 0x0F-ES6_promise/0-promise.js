f9e37ab9c29762217818280c6b1434b3fd2910fc
ecastan960 <ecastan1@eafit.edu.co>
2022-08-05 14:02:39 -0500

    [00] Task 0

---
+++ 0x0F-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI() {
+  let p = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if (a == 2) {
+      resolve('Success');
+    } else {
+      reject('Failed');
+    }
+  });
+  return p;
+}
