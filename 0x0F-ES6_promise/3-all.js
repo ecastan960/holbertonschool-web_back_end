79a1d55ae5f205e06ae3b49d571c502278be2308
ecastan960 <ecastan1@eafit.edu.co>
2022-08-06 16:26:01 -0500

    [08] Task 3

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const body = uploadPhoto();
-  const name = createUser();
-  let result = '';
-  body.then((text) => { result = text.body; });
-  body.catch(() => new Error('Signup system offline'));
-  name.then((text) => { result += ` ${text.firstName} ${text.lastName}`; console.log(result); });
-  name.catch(() => new Error('Signup system offline'));
+  try {
+    const body = uploadPhoto();
+    const name = createUser();
+    let result = '';
+    body.then((text) => { result = text.body; });
+    name.then((text) => { result += ` ${text.firstName} ${text.lastName}`; console.log(result); });
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
