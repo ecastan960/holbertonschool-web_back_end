fe1a5009aa058274c86e7cf59e2cf6b1252ce5f3
ecastan960 <ecastan1@eafit.edu.co>
2022-08-09 21:23:02 -0500

    [09] Task 8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString || !startString.length) return '';
   let result = '';
   set.forEach((element) => {
     if (element && element.startsWith(startString)) result += `${element.slice(startString.length)}-`;
