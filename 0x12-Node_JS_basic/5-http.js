6c0f1777fa89dc7ef462a3ccb5b5c818f7dd6777
ecastan960 <ecastan1@eafit.edu.co>
2022-08-15 23:12:52 -0500

    [14] task 5

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const countStudents = require('./3-read_file_async');
+const http = require('http');
+const args = process.argv.slice(2);
+const db = args[0];
+const port = 1245;
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(db);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
