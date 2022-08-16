f467059e706cd9704f922ae2de0c829af5e17b2c
ecastan960 <ecastan1@eafit.edu.co>
2022-08-15 21:34:09 -0500

    [07] task 3

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const headers = data.slice(0, data.indexOf('\n')).split(',');
+      const rows = data.slice(data.indexOf('\n') + 1).split('\n');
+      const array = rows.map((row) => {
+        const values = row.split(',');
+        const el = headers.reduce((object, header, index) => {
+          // eslint-disable-next-line
+          object[header] = values[index];
+          return object;
+        }, {});
+        return el;
+      });
+      console.log('Number of students:', array.length);
+      let countFieldCS = 0;
+      let countFieldSWE = 0;
+      let nameCS = 'List:';
+      let nameSWE = 'List:';
+      for (const student of array) {
+        if (student.field === 'CS') {
+          countFieldCS += 1;
+          nameCS = `${nameCS} ${student.firstname},`;
+        }
+        if (student.field === 'SWE') {
+          countFieldSWE += 1;
+          nameSWE = `${nameSWE} ${student.firstname},`;
+        }
+      }
+      console.log(`Number of students in CS: ${countFieldCS}. ${nameCS.slice(0, -1)}`);
+      console.log(`Number of students in SWE: ${countFieldSWE}. ${nameSWE.slice(0, -1)}`);
+      resolve(array);
+    });
+  });
+}
+
+module.exports = countStudents;
