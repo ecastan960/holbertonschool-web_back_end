218cdd5d54f93b21ccca550cf3a3213cf2e45e04
ecastan960 <ecastan1@eafit.edu.co>
2022-08-15 23:27:26 -0500

    [17] task

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const countStudents = require('./3-read_file_async');
+
+const args = process.argv.slice(2);
+const db = args[0];
+const app = express();
+const port = 1245;
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(db);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
