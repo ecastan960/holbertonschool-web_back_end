c2fac0393e3661f80f6b986438e7e86c8ed212ba
ecastan960 <ecastan1@eafit.edu.co>
2022-08-17 22:40:54 -0500

    [07] task 4

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -12,7 +12,7 @@ describe('sendPaymentRequestToApi', () => {
     let utils = sinon.stub(Utils, 'calculateNumber');
     utils.withArgs('SUM', 100, 20).returns(10);
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calledWith('The total is: 10')).to.be.true;
     expect(spy.calledOnce).to.be.true;
     spy.restore();
     utils.restore();
