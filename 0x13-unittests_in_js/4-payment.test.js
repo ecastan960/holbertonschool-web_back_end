b2d021adb582a1c96c7123bbc4b9490873d618bc
ecastan960 <ecastan1@eafit.edu.co>
2022-08-17 22:38:59 -0500

    [06] task 4

---
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+let chai = require('chai');
+let expect = chai.expect;
+const sinon = require('sinon');
+
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+  const spy = sinon.spy(Utils, 'calculateNumber');
+
+  it('validate Util function', () => {
+    let utils = sinon.stub(Utils, 'calculateNumber');
+    utils.withArgs('SUM', 100, 20).returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+    spy.restore();
+    utils.restore();
+  });
+});
