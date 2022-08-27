24e3c560cde6dfa6de8868378cfc4c92eab34591
ecastan960 <ecastan1@eafit.edu.co>
2022-08-26 23:03:53 -0500

    [06] test 6

---
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,16 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const data = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+};
+
+let job = queue
+  .create('push_notification_code', data)
+  .save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', () => console.log('Notification job completed'))
+  .on('failed', () => console.log('Notification job failed'));
