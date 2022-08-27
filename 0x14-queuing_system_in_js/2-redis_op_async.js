7dcca64b702f99f4b60de66a7a9d69a15ca684be
ecastan960 <ecastan1@eafit.edu.co>
2022-08-26 22:34:42 -0500

    [03] test 3

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    console.log(await get(schoolName));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
